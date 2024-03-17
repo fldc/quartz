25e32b93df50bd11b05b8166a7ddbf675d7eec96
Fredrik Storm <fredrik@fldc.se>
2024-03-17 12:59:09 +0100

    Quartz sync: Mar 17, 2024, 12:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "sv-SE",
+    locale: "en-US",
     baseUrl: "notes.fldc.se",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
