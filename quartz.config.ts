f2e35a32d2b6a6f9603382cb4aca341906151ac3
Fredrik Storm <fredrik@fldc.se>
2024-03-26 09:38:07 +0100

    update layout

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Notes of Doom",
+    pageTitle: "Notes of Doom",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
